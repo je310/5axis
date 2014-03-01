7ade26fe800345211b2c2e1c2f5ce7d3838d3cb1
Joshua Elsdon <je310@imperial.ac.uk>
2014-03-01 19:19:50 +0000

    starting to refactor, works in this state

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ using namespace irr;
 #include <IMeshWriter.h>
 #include <EMeshWriterEnums.h>
 
+#include "rotation.h"
+
 struct Tline{
 	core::vector3df start;
 	core::vector3df end;
@@ -416,7 +418,7 @@ void list::deleteList( struct node *ptr )
 }
 
 std::vector<Tline> allLines;
-std::vector<instruction> ReadInGCode(instruction savedVals);
+std::vector<instruction> ReadInGCode(instruction savedVals,char* argv);
 void faceTarget(Printnode& hero, scene::IAnimatedMeshSceneNode& target);
 void clear(char buffer[20]);
 bool notCollided = 1;
@@ -631,95 +633,95 @@ core::vector3df Headoffset(core::vector3df in,scene::ISceneNode* node){
 	return out;
 
 }
-core::vector3df getClosestPointOnLine
-( const core::vector3df& axis,const core::vector3df& pivot,const core::vector3df& point){
-    core::vector3df c = point - pivot;
-    f32 t = axis.dotProduct(c);
-    return pivot + axis*t;
-}
-
-//both axis and pivot are in world space
-void revolveNodeInWorldSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis, const core::vector3df& pivot){
-    node->updateAbsolutePosition();
-    core::vector3df p1 = node->getAbsolutePosition();
-    core::vector3df p2 = getClosestPointOnLine(axis, pivot, p1);
-    core::vector3df vect = p1 - p2;
-    
-    core::quaternion q;
-    q.fromAngleAxis(degs*core::DEGTORAD, axis);
-    q.getMatrix().rotateVect(vect);
-    
-    node->setPosition(p2 + vect);
-}
-
-void invrevolveNodeInWorldSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis, const core::vector3df& pivot){
-    node->updateAbsolutePosition();
-    core::vector3df p1 = node->getAbsolutePosition();
-    core::vector3df p2 = getClosestPointOnLine(axis, pivot, p1);
-    core::vector3df vect = p1 - p2;
-	core::CMatrix4<f32> mat ;
-    core::quaternion q;
-    q.fromAngleAxis(degs*core::DEGTORAD, axis);
-	q.getMatrix().getInverse(mat);
-	mat.rotateVect(vect);
-    
-    node->setPosition(p2 + vect);
-}
-
-void rotateNodeInLocalSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis)
-{
-    node->updateAbsolutePosition();
-    core::matrix4 m2 = node->getAbsoluteTransformation();
-    core::vector3df a = axis;
-    m2.rotateVect(a);
-    a.normalize();
- 
-    core::quaternion q;
-    q.fromAngleAxis(degs*core::DEGTORAD, a);
-    core::matrix4 m1 = q.getMatrix();
- 
-    core::matrix4 m = m1*m2;
-    node->setRotation(m.getRotationDegrees());
-}
-
-void invrotateNodeInLocalSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis)
-{
-    node->updateAbsolutePosition();
-    core::matrix4 m2 = node->getAbsoluteTransformation();
-    core::vector3df a = axis;
-    m2.rotateVect(a);
-    a.normalize();
- 
-    core::quaternion q;
-    q.fromAngleAxis(degs*core::DEGTORAD, a);
-    core::matrix4 m1 = q.getMatrix();
-	m1.getInverse(m1);
-    core::matrix4 m = m1*m2;
-    node->setRotation(m.getRotationDegrees());
-}
-
-void moveNodeInLocalSpace(scene::ISceneNode* node, const core::vector3df& distVect)
-{
-    node->updateAbsolutePosition();
-    core::matrix4 m = node->getAbsoluteTransformation();
-    core::vector3df d = distVect;
-    m.rotateVect(d);
-        
-    core::vector3df pos = node->getAbsolutePosition() + d;
-    node->setPosition(pos);
-}
-
-void revolveNodeInLocalSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis, const core::vector3df& pivot)
-{
-    moveNodeInLocalSpace(node, pivot);
-    rotateNodeInLocalSpace(node, degs, axis);
-    moveNodeInLocalSpace(node, -pivot);
-}
-
-void rotateNodeInWorldSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis,const core::vector3df& pivot){
-		revolveNodeInWorldSpace(node, degs, axis, pivot);
-		rotateNodeInLocalSpace(node, degs,axis);
-		}
+//core::vector3df getClosestPointOnLine
+//( const core::vector3df& axis,const core::vector3df& pivot,const core::vector3df& point){
+//    core::vector3df c = point - pivot;
+//    f32 t = axis.dotProduct(c);
+//    return pivot + axis*t;
+//}
+//
+////both axis and pivot are in world space
+//void revolveNodeInWorldSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis, const core::vector3df& pivot){
+//    node->updateAbsolutePosition();
+//    core::vector3df p1 = node->getAbsolutePosition();
+//    core::vector3df p2 = getClosestPointOnLine(axis, pivot, p1);
+//    core::vector3df vect = p1 - p2;
+//    
+//    core::quaternion q;
+//    q.fromAngleAxis(degs*core::DEGTORAD, axis);
+//    q.getMatrix().rotateVect(vect);
+//    
+//    node->setPosition(p2 + vect);
+//}
+//
+//void invrevolveNodeInWorldSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis, const core::vector3df& pivot){
+//    node->updateAbsolutePosition();
+//    core::vector3df p1 = node->getAbsolutePosition();
+//    core::vector3df p2 = getClosestPointOnLine(axis, pivot, p1);
+//    core::vector3df vect = p1 - p2;
+//	core::CMatrix4<f32> mat ;
+//    core::quaternion q;
+//    q.fromAngleAxis(degs*core::DEGTORAD, axis);
+//	q.getMatrix().getInverse(mat);
+//	mat.rotateVect(vect);
+//    
+//    node->setPosition(p2 + vect);
+//}
+//
+//void rotateNodeInLocalSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis)
+//{
+//    node->updateAbsolutePosition();
+//    core::matrix4 m2 = node->getAbsoluteTransformation();
+//    core::vector3df a = axis;
+//    m2.rotateVect(a);
+//    a.normalize();
+// 
+//    core::quaternion q;
+//    q.fromAngleAxis(degs*core::DEGTORAD, a);
+//    core::matrix4 m1 = q.getMatrix();
+// 
+//    core::matrix4 m = m1*m2;
+//    node->setRotation(m.getRotationDegrees());
+//}
+//
+//void invrotateNodeInLocalSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis)
+//{
+//    node->updateAbsolutePosition();
+//    core::matrix4 m2 = node->getAbsoluteTransformation();
+//    core::vector3df a = axis;
+//    m2.rotateVect(a);
+//    a.normalize();
+// 
+//    core::quaternion q;
+//    q.fromAngleAxis(degs*core::DEGTORAD, a);
+//    core::matrix4 m1 = q.getMatrix();
+//	m1.getInverse(m1);
+//    core::matrix4 m = m1*m2;
+//    node->setRotation(m.getRotationDegrees());
+//}
+//
+//void moveNodeInLocalSpace(scene::ISceneNode* node, const core::vector3df& distVect)
+//{
+//    node->updateAbsolutePosition();
+//    core::matrix4 m = node->getAbsoluteTransformation();
+//    core::vector3df d = distVect;
+//    m.rotateVect(d);
+//        
+//    core::vector3df pos = node->getAbsolutePosition() + d;
+//    node->setPosition(pos);
+//}
+//
+//void revolveNodeInLocalSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis, const core::vector3df& pivot)
+//{
+//    moveNodeInLocalSpace(node, pivot);
+//    rotateNodeInLocalSpace(node, degs, axis);
+//    moveNodeInLocalSpace(node, -pivot);
+//}
+//
+//void rotateNodeInWorldSpace(scene::ISceneNode* node, f32 degs, const core::vector3df& axis,const core::vector3df& pivot){
+//		revolveNodeInWorldSpace(node, degs, axis, pivot);
+//		rotateNodeInLocalSpace(node, degs,axis);
+//		}
 
 struct v3 { 
     float x, y, z; 
@@ -1082,8 +1084,13 @@ void mirrorx(scene::IAnimatedMeshSceneNode* obj){ // this is neccessary due to f
 
 
 
-int main()
+int main(int argc, char* argv[])
 {
+	char callslicer[50]  = "slic3r --load stl/config.ini stl/";
+	strcat(callslicer,argv[1]);
+	strcat(callslicer,".stl");
+	system (callslicer);
+
 	float CurrentA = 0; 
 	float CurrentB = 0 ; 
 	omp_set_num_threads(4);
@@ -1167,7 +1174,7 @@ int main()
 	savedVals.E = 0;
 
 	std::vector<instruction> instructions;
-	instructions = ReadInGCode(savedVals);
+	instructions = ReadInGCode(savedVals, argv[1]);
 	numberOfInstructions = instructions.size();
 
 	driver->setTransform(video::ETS_WORLD,  core::matrix4());
@@ -1198,8 +1205,11 @@ int main()
 
 	io::IWriteFile *out = device->getFileSystem()->createAndWriteFile("newone.stl");
 
+	char path[30] = "stl/";
+	strcat(path,argv[1]);
+	strcat(path,".stl");
 	scene::IAnimatedMeshSceneNode* thing = 0;
-	thing = smgr->addAnimatedMeshSceneNode(smgr->getMesh("stl/test.stl"),
+	thing = smgr->addAnimatedMeshSceneNode(smgr->getMesh(path),
 						0, ID_IsNotPickable);
 	mirrorx(thing);
 	thing->setMaterialType(video::EMT_TRANSPARENT_ALPHA_CHANNEL);
@@ -1210,6 +1220,8 @@ int main()
 	pBuffer = thing->getMesh()->getMeshBuffer(0);
 	bool success = writer->writeMesh(out,mymesh);
 	writestl(pBuffer, "shit");
+	
+	
 
 	core::vector3df midpointtest = findmodeloffset(thing, 0,0, 0);
 	thing->setPosition(thing->getAbsolutePosition()-midpointtest);
@@ -1574,9 +1586,12 @@ int main()
 }
 
 
-std::vector<instruction> ReadInGCode(instruction savedVals){
+std::vector<instruction> ReadInGCode(instruction savedVals, char* argv){
 	using namespace std;
-	ifstream infile("IN.gcode");
+	char gcode[40] ="stl/";
+	strcat(gcode,argv);
+	strcat(gcode,".gcode");
+	ifstream infile(gcode);
 	string line;
 	string unit;
 	char buffer[20];
