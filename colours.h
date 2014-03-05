dad435d202bb2ab09ca6ae55abbdbad61b6a4ce5
Joshua Elsdon <je310@imperial.ac.uk>
2014-03-05 02:30:14 +0000

    stray files added

---
+++ colours.h
@@ -0,0 +1,15 @@
+#ifndef __COLS_INCLUDED__   // if x.h hasn't been included yet...
+#define __COLS_INCLUDED__ 
+#include <irrlicht.h>
+
+using namespace irr;
+
+//video::SColor greencol  =	video::SColor(255,34,177,76);
+//video::SColor pinkcol  =	video::SColor(255,255,174,201);
+//video::SColor bluecol  =	video::SColor(255,63,72,204);
+//video::SColor redcol  =		video::SColor(255,237,28,36);
+//video::SColor yellowcol  =  video::SColor(255,255,201,14);
+
+
+
+#endif
